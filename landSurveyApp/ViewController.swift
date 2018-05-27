22847a41dd4e7528da30b90c4f1c08207a5d4a76
Jay Letheby <jay.letheby@gmail.com>
2018-05-27 19:21:39 +1000

    Initial Commit

---
+++ landSurveyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  landSurveyApp
+//
+//  Created by Jay Letheby on 27/5/18.
+//  Copyright © 2018 Jay Letheby solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
